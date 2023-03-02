a8bca2cb268934ee3c21e044974c36fb2cfc2797
lk1u20 <lk1u20@soton.ac.uk>
2023-03-02 18:05:39 +0000

    changes to Calculator

--- src/main/java/com/team44/runwayredeclarationapp/controller/Calculator.java
+++ src/main/java/com/team44/runwayredeclarationapp/controller/Calculator.java
@@ -1,4 +1,4 @@
-package com.team44.runwayredeclarationapp.utility;
+package com.team44.runwayredeclarationapp.controller;
 import com.team44.runwayredeclarationapp.model.Aircraft;
 import com.team44.runwayredeclarationapp.model.Airport;
 import com.team44.runwayredeclarationapp.model.Obstacles;
@@ -8,35 +8,41 @@ import java.util.List;
 public class Calculator {
 
   public static double calcLandingTowardsObs(Airport airport, Aircraft aircraft,Obstacles obstacle){
+    List<Runway> rws = airport.getRunways();
+    double pos = obstacle.getPositionR();
+    double resa = rws.get(1).getResa();
+    double finalLDA;
 
-    return 1;
+    finalLDA = pos - resa - 60;
+
+  return finalLDA;
   }
 
   public static double calcLandingAwayFromObs(Airport airport, Aircraft aircraft, Obstacles obstacle){
     List<Runway> rws = airport.getRunways();
     double h = obstacle.getHeight();
-    double pos = obstacle.getPosition();
+    double pos = obstacle.getPositionL();
     double bp = aircraft.getBlastProt();
     double lda = rws.get(1).getLDA();
     double resa = rws.get(1).getResa();
-    double finalValue;
+    double finalLDA;
 
     if (resa < bp){
       if (bp < h * 50) {
-        finalValue = lda - pos - (h * 50) - 60;
+        finalLDA = lda - pos - (h * 50) - 60;
       } else {
-        finalValue = lda - pos - bp;
+        finalLDA = lda - pos - bp;
       }
     } else {
       if (resa < h * 50) {
-        finalValue = lda - pos - (h * 50) - 60;
+        finalLDA = lda - pos - (h * 50) - 60;
       } else {
-        finalValue = lda - pos - resa - 60;
+        finalLDA = lda - pos - resa - 60;
       }
 
     }
 
-    return finalValue;
+    return finalLDA;
   }
 
   public static double calcTakeOffTowardsObs(Airport airport, Obstacles obstacle){
