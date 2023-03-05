59d409c414b173de85c6d1980ad8d8de82bb39b2
lk1u20 <lk1u20@soton.ac.uk>
2023-03-05 21:47:15 +0000

    changes to calculator

--- src/main/java/com/team44/runwayredeclarationapp/controller/Calculator.java
+++ src/main/java/com/team44/runwayredeclarationapp/controller/Calculator.java
@@ -1,16 +1,13 @@
 package com.team44.runwayredeclarationapp.controller;
 import com.team44.runwayredeclarationapp.model.Aircraft;
-import com.team44.runwayredeclarationapp.model.Airport;
 import com.team44.runwayredeclarationapp.model.Obstacles;
 import com.team44.runwayredeclarationapp.model.Runway;
-import java.util.List;
 
 public class Calculator {
 
-  public static double calcLandingTowardsObs(Airport airport, Aircraft aircraft,Obstacles obstacle){
-    List<Runway> rws = airport.getRunways();
+  public static double calcLandingTowardsObs(Runway rw, Obstacles obstacle){
     double pos = obstacle.getPositionR();
-    double resa = rws.get(1).getResa();
+    double resa = rw.getResa();
     double finalLDA;
 
     finalLDA = pos - resa - 60;
@@ -18,13 +15,12 @@ public class Calculator {
   return finalLDA;
   }
 
-  public static double calcLandingAwayFromObs(Airport airport, Aircraft aircraft, Obstacles obstacle){
-    List<Runway> rws = airport.getRunways();
+  public static double calcLandingAwayFromObs(Runway rw, Aircraft aircraft, Obstacles obstacle){
     double h = obstacle.getHeight();
     double pos = obstacle.getPositionL();
     double bp = aircraft.getBlastProt();
-    double lda = rws.get(1).getLDA1();
-    double resa = rws.get(1).getResa();
+    double lda = rw.getLDA1();
+    double resa = rw.getResa();
     double finalLDA;
 
     if (resa < bp){
@@ -45,13 +41,62 @@ public class Calculator {
     return finalLDA;
   }
 
-  public static double calcTakeOffTowardsObs(Airport airport, Obstacles obstacle){
+  public static Runway calcTakeOffTowardsObs(Runway rw, Obstacles obstacle){
+    double h = obstacle.getHeight();
+    double pos = obstacle.getPositionL();
+    double resa = rw.getResa();
 
-    return 1;
+    double finalValue = pos - (h * 50) - resa;
+    double[] newParams = new double[15];
+
+    newParams[0] = rw.getRunwayL();
+    newParams[1] = rw.getRunwayW();
+    newParams[2] = rw.getStripL();
+    newParams[3] = rw.getStripW();
+    newParams[4] = rw.getClearwayW();
+    newParams[5] = finalValue;
+    newParams[6] = finalValue;
+    newParams[7] = finalValue;
+    newParams[8] = rw.getLDA1();
+    newParams[9] = rw.getDisThresh1();
+    newParams[10] = rw.getTORA2();
+    newParams[11] = rw.getLDA1();
+    newParams[12] = rw.getDisThresh1();
+    newParams[13] = rw.getTORA2();
+    newParams[14] = rw.getLDA1();
+    newParams[15] = rw.getDisThresh1();
+
+    rw.updateParameters(newParams);
+    return rw;
   }
 
-  public static double calcTakeOffAwayFromObs(Airport airport, Obstacles obstacle){
+  public static Runway calcTakeOffAwayFromObs(Runway rw, Aircraft aircraft, Obstacles obstacle){
+    double h = obstacle.getHeight();
+    double pos = obstacle.getPositionL();
+    double bp = aircraft.getBlastProt();
+    double resa = rw.getResa();
 
-    return 1;
+    double finalValue = pos - (h * 50) - resa;
+    double[] newParams = new double[15];
+
+    newParams[0] = rw.getRunwayL();
+    newParams[1] = rw.getRunwayW();
+    newParams[2] = rw.getStripL();
+    newParams[3] = rw.getStripW();
+    newParams[4] = rw.getClearwayW();
+    newParams[5] = finalValue;
+    newParams[6] = finalValue;
+    newParams[7] = finalValue;
+    newParams[8] = rw.getLDA1();
+    newParams[9] = rw.getDisThresh1();
+    newParams[10] = rw.getTORA2();
+    newParams[11] = rw.getLDA1();
+    newParams[12] = rw.getDisThresh1();
+    newParams[13] = rw.getTORA2();
+    newParams[14] = rw.getLDA1();
+    newParams[15] = rw.getDisThresh1();
+
+    rw.updateParameters(newParams);
+    return rw;
   }
 }
