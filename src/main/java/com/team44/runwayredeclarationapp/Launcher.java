0c59887e5cb44fe71653199013e1fb4ab811b588
Alvis <37454764+avibn@users.noreply.github.com>
2023-03-08 21:50:56 +0000

    add shade plugin

---
+++ src/main/java/com/team44/runwayredeclarationapp/Launcher.java
@@ -0,0 +1,17 @@
+package com.team44.runwayredeclarationapp;
+
+/**
+ * Launcher for the shaded jar file
+ */
+public class Launcher {
+
+    /**
+     * Launch the JavaFX application
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        App.main(args);
+    }
+
+}
